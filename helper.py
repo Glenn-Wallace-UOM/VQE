d4a46cfd287ed450b2a2d796fb57bdae07bb8c60
Glenn Wallace <34867563+Dougvacs@users.noreply.github.com>
2022-11-14 01:19:35 +0100

    Writing parameters to files

---
+++ helper.py
@@ -0,0 +1,87 @@
+import openfermion as of
+import numpy as np
+import scipy as sp
+import qulacs as qlcs
+
+def create_iswap_gate(target1, target2, angle):
+    half_angle = angle/2
+    sin_ha = np.sin(half_angle)
+    cos_ha = np.cos(half_angle)
+    iswap_gate_matrix = [
+        [1,0,0,0],
+        [0,cos_ha, -1j*sin_ha,0],
+        [0,-1j*sin_ha, cos_ha,0],
+        [0,0,0,1]]
+    iswap_gate = qlcs.gate.DenseMatrix(
+        [target1, target2], iswap_gate_matrix)
+    return iswap_gate
+
+def add_param_iswap_gate(circuit, target1, target2):
+    circuit.add_RZ_gate(target1, -np.pi/2)
+    circuit.add_RZ_gate(target2, np.pi/2)
+    circuit.add_sqrtX_gate(target1)
+    circuit.add_RZ_gate(target1, np.pi/2)
+    circuit.add_CNOT_gate(target1, target2)
+    circuit.add_sqrtX_gate(target1)
+    circuit.add_sqrtX_gate(target2)
+    circuit.add_parametric_RZ_gate(target1, np.pi - 0.5*5)
+    circuit.add_parametric_RZ_gate(target2, np.pi - 0.5*5)
+    circuit.add_sqrtX_gate(target1)
+    circuit.add_sqrtX_gate(target2)
+    circuit.add_RZ_gate(target1, 7*np.pi/2)
+    circuit.add_RZ_gate(target2, 3*np.pi)
+    circuit.add_CNOT_gate(target1, target2)
+    circuit.add_sqrtX_gate(target1)
+    circuit.add_RZ_gate(target2, -np.pi/2)
+    circuit.add_RZ_gate(target1, -np.pi/2)
+
+def add_param_crz_gate(circuit, target, control):
+    circuit.add_parametric_RZ_gate(control, np.pi/2)
+    circuit.add_CNOT_gate(target, control)
+    circuit.add_parametric_RZ_gate(control, -np.pi/2)
+    circuit.add_CNOT_gate(target, control)
+
+def create_n_iswap_gate(targets, angle):
+    n = len(targets)
+    half_angle = angle/2
+    sin_ha = np.sin(half_angle)
+    cos_ha = np.cos(half_angle)
+    iswap_gate_matrix = [
+        [1,0,0,0],
+        [0,cos_ha, -1j*sin_ha,0],
+        [0,-1j*sin_ha, cos_ha,0],
+        [0,0,0,1]]
+    n_iswap_gate = []
+    n_iswap_gate = qlcs.gate.merge([
+        qlcs.gate.DenseMatrix([targets[i], targets[i+1]], iswap_gate_matrix)
+        for i in range(0, n-1)
+    ])
+    return n_iswap_gate
+
+def create_crz_gate(control, target, angle):
+    rz_gate = qlcs.gate.ParametricRZ(target, angle)
+    crz_mat_gate = qlcs.gate.to_matrix_gate(rz_gate)
+    crz_mat_gate.add_control_qubit(control, 0)
+    return crz_mat_gate
+
+def create_qiskit_crz_gate(control, target, angle):
+    rz_gate = create_qiskit_rz_gate(target, angle)
+    crz_gate = qlcs.gate.to_matrix_gate(rz_gate)
+    crz_gate.add_control_qubit(control, 1)
+    return crz_gate
+
+def create_n_rz_gate(self, targets, angle):
+    rz_gates = []
+    for t in targets:
+        rz_gates.append(qlcs.gate.RZ(t, angle))
+    rz_gates = qlcs.gate.merge(rz_gates)
+    return rz_gates
+
+def create_qiskit_rz_gate(target, angle):
+    rz_gate_matrix = [
+        [np.exp(-1j*(angle/2)), 0],
+        [0, np.exp(1j*(angle/2))]
+    ]
+    rz_gate = qlcs.gate.DenseMatrix(
+        target, rz_gate_matrix)
+    return rz_gate
--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import scipy as sp
 import qulacs as qlcs
 import matplotlib.pyplot as plt
 import qulacsvis as qlcsvis
+from time import time
+
+from helper import *
 
 ## REFACTOR LATER
 class VQE():
     # t hopping terms, V coulomb, U onsite, N number of spins, L number of sites
-    def __init__(self, shape, U, V, t, layers, display_ansatz):
+    def __init__(self, shape, U, V, t, layers, maxiter, display_ansatz):
         # Init some instance vars
         self.N = len(shape)
         self.L = len(shape[0])
@@ -18,22 +21,23 @@ class VQE():
             for site in colour:
                 self.Np += site
         self.shape = sum(shape, ())
-        
         self.n_qubits = self.L*self.N # Calculate number of qubits
         self.hamiltonian = self.gen_hamiltonian(U, V, -t) # Generate hamiltonian
         self.layers = layers # Set layers
         # Set circuit parameter count
-        self.param_count = self.param_count = (3*self.N*self.L - self.N - self.L)*self.layers + self.Np
-        # Set display ansatz flag
+        self.param_count = (3*self.N*self.L - self.N - self.L)*self.layers + self.Np
+        # self.param_count = 3 + (6 + 9)*self.layers
         self.display_ansatz = display_ansatz
         # Init iteration counter
         self.iteration_counter = 0
         # Clear/Create output file
-        self.f = open("test", "w").close()
+        self.f = open("vqe_iterations", "w").close()
+        # Set maximum iteration count
+        self.maxiter = maxiter
     
     def gen_hamiltonian(self, U, V, t):
         # Generate openfermion hamiltonian hopping and onsite terms
-        print("Generating OpenFermion hamiltonian...")
+        # print("Generating OpenFermion hamiltonian...")
         n_qubits = self.n_qubits
         hopping_terms = []
         hopping_terms_conj = []
@@ -80,19 +84,10 @@ class VQE():
             )
 
         of_hamiltonian = sum(hopping_terms) + sum(onsite_terms) + sum(density_density_terms)
-        print("Hopping Terms:")
-        print(hopping_terms)
-        print("Onsite Terms:")
-        print(onsite_terms)
-        print("Density-Density Terms:")
-        print(density_density_terms)
-        
-        # Apply jordan wigner transformation
-        print("Applying Jordan Wigner transformation...")
+
         of_jw_hamiltonian = of.transforms.jordan_wigner(of_hamiltonian)
 
         # Convert to Qulacs hamiltonian
-        print("Converting to Qulacs hamiltonian...")
         qlcs_hamiltonian = qlcs.observable.create_observable_from_openfermion_text(
             str(of_jw_hamiltonian)
         )
@@ -102,66 +97,62 @@ class VQE():
     # Ansatz circuit, layers -> repetitions
     def gen_ansatz(self, theta_list):
         # Used to index the theta_list
-        param_countdown = self.param_count-1
+        param_countdown = 0
         # Define initialiser circuit
         circuit = qlcs.QuantumCircuit(self.n_qubits)
         for i in range(0, self.n_qubits):
             if self.shape[i] == 1:
                 circuit.add_X_gate(i)
                 circuit.add_RZ_gate(i, theta_list[param_countdown])
-                param_countdown -= 1
-        # Calculate iswap_targets
-        iswap_targets = []
-        for site in range(0, self.L-1):
-            for colour in range(0, self.n_qubits, self.N):
-                iswap_targets.append((site+colour, site+colour+1))
+                param_countdown += 1
         # Define ansatz circuit
         for l in range(0, self.layers):
-            # Add iSWAP gates to ansatz
+            # Add adjacent iSWAP gates to ansatz
             for site in range(0, self.L-1):
                 for colour in range(0, self.n_qubits, self.L):
                     curr_qubit = site+colour
                     next_qubit = curr_qubit + 1
-                    circuit.add_gate(self.create_iswap_gate(
-                            curr_qubit, next_qubit, theta_list[param_countdown]
+                    circuit.add_gate(create_n_iswap_gate(
+                            [curr_qubit, next_qubit], theta_list[param_countdown]
                         ))
-                    param_countdown -= 1
-            # Add controlled RZ gates to ansatz
+                    param_countdown += 1
             for i in range(0, self.n_qubits-self.N):
-                circuit.add_gate(self.create_crz_gate(i, i+self.N, theta_list[param_countdown]))
-                param_countdown -= 1
+                # circuit.add_gate(create_crz_gate(i, i+self.N, theta_list[param_countdown]))
+                circuit.add_gate(create_qiskit_crz_gate(i, i+self.N, theta_list[param_countdown]))
+                param_countdown += 1
             # Add RZ gates to ansatz
             for i in range(0, self.n_qubits):
-                circuit.add_gate(qlcs.gate.RZ(i, theta_list[param_countdown]))
-                param_countdown -= 1
-            # Save ansatz if flag is set
+                # circuit.add_gate(qlcs.gate.RZ(i, theta_list[param_countdown]))
+                circuit.add_gate(create_qiskit_rz_gate(i, theta_list[param_countdown]))
+                param_countdown += 1
             if self.display_ansatz:
-                print("Ansatz circuit:")
+                # print("Ansatz circuit:")
                 qlcsvis.circuit_drawer(circuit, "mpl")
                 self.display_ansatz = False
+        # print(self.param_count-param_countdown)
         return circuit
 
     #  Cost function
     def cost(self, theta_list):
         state = qlcs.QuantumState(self.n_qubits) # Define all 0 state
-        circuit = self.gen_ansatz(theta_list) # Generate circuits using theta list
-        circuit.update_quantum_state(state) # Pass state through ansatz circuits
+        ansatz = self.gen_ansatz(theta_list)
+        ansatz.update_quantum_state(state)
         return self.hamiltonian.get_expectation_value(state)
 
     def run(self):
-        print("Running VQE...")
+        # print("Running VQE...")
         self.cost_history = []
         init_theta_list = np.random.rand(self.param_count)*2*np.pi
         self.cost_history.append(self.cost(init_theta_list))
         method = "BFGS"
-        options = {"disp":True, "maxiter":1000}
+        options = {"disp":True, "maxiter":self.maxiter}
         # Define optimiser callback function
         def optimiser_callback(x):
             cost_value = self.cost(x)
-            self.cost_history.append(cost_value)
+            self.cost_history.append((x, cost_value))
             self.iteration_counter += 1
             #print("Iteration", str(self.iteration_counter)+":", cost_value)
-            with open("test", "a") as f:
+            with open("vqe_iterations", "a") as f:
                 f.write("Iteration "+str(self.iteration_counter)+": "+str(cost_value))
                 f.write("\n")
         # Run optimiser
@@ -171,24 +162,19 @@ class VQE():
         )
         return self.cost_history
 
-    def create_iswap_gate(self, target1, target2, angle):
-        iswap_gate_matrix = [
-            [1,0,0,0],
-            [0,np.cos(angle/2), -1j*np.sin(angle/2),0],
-            [0,-1j*np.sin(angle/2), np.cos(angle/2),0],
-            [0,0,0,1]]
-        iswap_gate = qlcs.gate.DenseMatrix(
-            [target1, target2], iswap_gate_matrix)
-        return iswap_gate
-    
-    def create_crz_gate(self, control, target, angle):
-        rz_gate = qlcs.gate.ParametricRZ(target, angle)
-        crz_mat_gate = qlcs.gate.to_matrix_gate(rz_gate)
-        crz_mat_gate.add_control_qubit(control, 0)
-        return crz_mat_gate
-    
 if __name__ == "__main__":
-    shape = ((1, 0, 0, 0), (1, 0, 0, 0), (1, 0, 0, 0), (1, 0, 0, 0))
-    #shape = ((1, 0, 0),(0, 1, 0),(0, 0, 1))
-    vqe = VQE(shape=shape, U=1, V=0, t=1, layers=3, display_ansatz=True)
-    results = vqe.run()
+    #run_time = time()
+    shape = ((1, 0, 0),(1, 0, 0),(1, 0, 0))
+    #shape = list((1,0,0,0) for i in range(4))
+    vqe = VQE(shape=shape, U=5, V=10, t=1, layers=3, maxiter=100000, display_ansatz=True)
+    #results = vqe.run()
+    #run_time = time() - run_time
+    #print("Run time:", run_time, "seconds")
+
+    #best_result = vqe.cost(results[len(results)-1][0])
+    #best_params = results[len(results)-1][0]
+
+    #np.save("TN3U5V10L3.npy", best_params)
+    best = np.load("N3U5V10L3.npy")
+
+    print(vqe.cost(best))
